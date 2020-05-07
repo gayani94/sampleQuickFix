ddfe42af3ccc89f412804ea844fa31cff21b97e7
Gayani Nadeesha <gayani.nadee@yahoo.com>
2020-05-07 20:47:02 +0900

    first commit

---
+++ QuickFixJInitiator/src/main/java/com/test/quickfix/TradeInitiator.java
@@ -0,0 +1,44 @@
+package com.test.quickfix;
+
+import quickfix.*;
+
+/**
+ * Created by Gayani on 04/13/2020.
+ */
+public class TradeInitiator extends MessageCracker implements Application{
+    @Override
+    public void onCreate(SessionID sessionID) {
+
+    }
+
+    @Override
+    public void onLogon(SessionID sessionID) {
+
+    }
+
+    @Override
+    public void onLogout(SessionID sessionID) {
+
+    }
+
+    @Override
+    public void toAdmin(Message message, SessionID sessionID) {
+
+    }
+
+    @Override
+    public void fromAdmin(Message message, SessionID sessionID) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, RejectLogon {
+        System.out.println("Admin Message Received (Initiator) :" + message.toString());
+    }
+
+    @Override
+    public void toApp(Message message, SessionID sessionID) throws DoNotSend {
+
+    }
+
+    @Override
+    public void fromApp(Message message, SessionID sessionID) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, UnsupportedMessageType {
+        System.out.println("Application Response Received (Initiator) :" +  message.toString());
+
+    }
+}
